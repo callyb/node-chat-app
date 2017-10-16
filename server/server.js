3b13e0c079d2f937b5efab6fa09c947dd75bd689
CallyB <carole.nissen@gmail.com>
2017-10-16 19:13:02 +0100

    message system established

--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on("connection", socket => {
   console.log("new user connected");
 
+  socket.emit("newMessage", {
+    from: "Admin",
+    text: "Welcome to the app"
+  });
+
+  socket.broadcast.emit("newMessage", {
+    from: "NewUser",
+    text: "New User joined"
+  });
+
   // Listener for message to browser from server
   socket.on("createMessage", message => {
     console.log("createMessage", message);
@@ -22,6 +32,12 @@ io.on("connection", socket => {
       text: message.text,
       createdAt: new Date().getTime()
     });
+
+    // socket.broadcast.emit("newMessage", {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on("disconnect", () => {
